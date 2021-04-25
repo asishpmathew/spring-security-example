e47f83f99363820e9774584d54f96aabaebda6b0
asishpmathew <asishpmathew@gmail.com>
2021-04-25 18:51:45 +0530

    ant matchers to bypass security+in memory user

--- src/main/java/com/asish/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/asish/springsecurity/security/SecurityConfig.java
@@ -1,13 +1,37 @@
 package com.asish.springsecurity.security;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails test = User.builder()
+                .username("test")
+                .password(passwordEncoder.encode("test"))
+                .roles("studentt")
+                .build();
+        return new InMemoryUserDetailsManager(test);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
